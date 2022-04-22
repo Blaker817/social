9d314dafabd1b7d8baf516ab698a7376fd6ec1ce
Blake Elliott <blakeelliott@Blakes-MacBook-Pro.local>
2022-04-22 10:39:00 -0500

    send indexdb to db

--- Develop/public/js/indexeddb.js
+++ Develop/public/js/indexeddb.js
@@ -3,10 +3,43 @@ const dbOpenRequest = indexedDB.open("expenses", 1)
 
 dbOpenRequest.addEventListener("upgradeneeded", (e) => {
     const db = e.target.result
-    db.createObjectStore("transaction", { autoIncrement: true })
+    db.createObjectStore("expense", { autoIncrement: true })
 })
 
 dbOpenRequest.addEventListener("success", (e) => {
     db = e.target.result
-    // upload 
+    console.log("success")
+    sendToDb()
 })
+function sendToDb() {
+    const expense = db.transaction("expense", "readwrite")
+    const expenseObjectStore = expense.objectStore("expense")
+    const records = expenseObjectStore.getAll()
+    records.onsuccess = () => {
+        if (records.result.length) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(records.result),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+                .then((response) => response.json())
+                .then(() => {
+                    const expense = db.transaction("expense", "readwrite")
+                    const expenseObjectStore =
+                        expense.objectStore("expense");
+                    expenseObjectStore.clear();
+                    //alert
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    }
+}
+function saveRecord(record) {
+    const expense = db.transaction("expense", "readwrite")
+    const expenseObjectStore = expense.objectStore("expense")
+    expenseObjectStore.add(record)
+}
